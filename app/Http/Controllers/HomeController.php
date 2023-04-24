67471919ab6e7cda654dbacfd1ad32550caa7862
sagarmintu <sagarskbehera1996@gmail.com>
2023-04-24 08:53:14 +0530

    display data in food menu place

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,15 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+use App\Models\User;
+use App\Models\Food;
+
 class HomeController extends Controller
 {
     public function index()
     {
-        return view("home");
+        $data = food::all();
+        return view("home", compact("data"));
     }
 
     public function redirects()
     {
+        $data = food::all();
         $usertype = Auth::user()->usertype;
 
         if ($usertype == '1') 
@@ -22,7 +27,7 @@ class HomeController extends Controller
         } 
         else 
         {
-            return view("home");
+            return view("home", compact("data"));
         }
         
     }
